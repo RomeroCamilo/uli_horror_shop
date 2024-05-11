ad5461e1531dd82fb5293dc050038b87120ccf78
Camilo Romero <romerocamilo24@gmail.com>
2024-05-11 19:18:34 -0400

    sql server

--- index.php
+++ index.php
@@ -1,3 +1,29 @@
 <?php
-echo "Hello, World!";
+$servername = "romerocamilo.database.windows.net";
+$username = "camilo"; // Default username for MySQL in XAMPP
+$password = "Champ2118!";     // No password by default
+$database = "halloween_shop"; //database name
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Redirect to Sign Up</title>
+</head>
+<body>
+    <h2>Connection Successful!</h2>
+    <p>Your connection to the database was successful.</p>
+    <button onclick="window.location.href='signup.php'">Sign Up</button>
+</body>
+</html>
