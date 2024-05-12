894b52f5611265f60a79073c5923a2ef95ccdec8
Camilo Romero <romerocamilo24@gmail.com>
2024-05-11 20:18:59 -0400

    test

--- index.php
+++ index.php
@@ -4,13 +4,7 @@ $username = "root";
 $password = "Champ2118!";
 $database = "halloween_shop";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $database);
 
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
 
 echo "Connected successfully";
 ?>
