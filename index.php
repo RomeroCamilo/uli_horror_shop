bbc1d932d933f9f644402c25e5875386811bcc24
Camilo Romero <romerocamilo24@gmail.com>
2024-05-11 20:16:02 -0400

    test

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "34.31.197.231";
-$username = "root"; // Default username for MySQL in XAMPP
-$password = "Champ2118!";     // No password by default
-$database = "halloween_shop"; //database name
+$servername = "your_cloud_mysql_host";
+$username = "root";
+$password = "Champ2118!";
+$database = "halloween_shop";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $database);
@@ -11,19 +11,6 @@ $conn = new mysqli($servername, $username, $password, $database);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+
 echo "Connected successfully";
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Redirect to Sign Up</title>
-</head>
-<body>
-    <h2>Connection Successful!</h2>
-    <p>Your connection to the database was successful.</p>
-    <button onclick="window.location.href='signup.php'">Sign Up</button>
-</body>
-</html>
