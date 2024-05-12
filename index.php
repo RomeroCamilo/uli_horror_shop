c8e026d031159340ffefd3aba57bb0b4b471de87
Camilo Romero <romerocamilo24@gmail.com>
2024-05-11 20:24:06 -0400

    test

--- index.php
+++ index.php
@@ -1,10 +1,16 @@
 <?php
-$servername = "your_cloud_mysql_host";
-$username = "root";
-$password = "Champ2118!";
-$database = "halloween_shop";
+$servername = "34.31.197.231"; // Replace with your MySQL server's IP address or hostname
+$username = "root"; // MySQL username
+$password = "Champ2118!"; // MySQL password
+$database = "halloween_shop"; // Name of the MySQL database you want to connect to
 
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database);
 
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 
 echo "Connected successfully";
 ?>
